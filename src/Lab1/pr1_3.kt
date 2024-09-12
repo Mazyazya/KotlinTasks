0ab95f3b1ac08e36e1bb94301164af727c3a2634
Mazyazya <166235469+Mazyazya@users.noreply.github.com>
2024-09-12 16:37:51 +0900

    Kotlin

--- src/Lab1/pr1_3.kt
+++ src/Lab1/pr1_3.kt
@@ -3,6 +3,6 @@ package Lab1
 import kotlin.math.exp
 
 fun main() {
-    var num = String.format("%.3f", exp(1.0)).toDouble()
+    val num = String.format("%.3f", exp(1.0)).toDouble()
     println("Your number is $num")
 }
