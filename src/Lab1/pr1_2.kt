58bb1ac5645f6d642907469e8c8e50acf681adce
Mazyazya <166235469+Mazyazya@users.noreply.github.com>
2024-09-22 19:35:10 +0900

    Kotlin Lab1

--- src/Lab1/pr1_2.kt
+++ src/Lab1/pr1_2.kt
@@ -3,6 +3,6 @@ package Lab1
 fun main() {
     print("Enter a number: ")
     var num = readln().toDouble()
-    num = String.format("%.3f", num).toDouble()
-    println("Your number is $num")
+    println("Your number is ${String.format("%.3f", num)}")
 }
+
--- src/Lab1/pr1_3.kt
+++ src/Lab1/pr1_3.kt
@@ -3,6 +3,6 @@ package Lab1
 import kotlin.math.exp
 
 fun main() {
-    val num = String.format("%.3f", exp(1.0)).toDouble()
+    val num = String.format("%.3f", exp(1.0))
     println("Your number is $num")
 }
