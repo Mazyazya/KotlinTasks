1ef63516854a405e4f567ac0e851e0ed6ca376bc
Mazyazya <ilya.mazza.06@gmail.com>
2024-09-29 18:52:00 +0900

    Kotlin Lab6

--- src/Lab6/pr6_30.kt
+++ src/Lab6/pr6_30.kt
@@ -5,7 +5,7 @@ import kotlin.random.Random
 fun main() {
     val N = 100
     val arr = Array<Int>(N, { Random.nextInt(0, 100) })
-    println("Выводим массив?: ")
+    println("Массив: ")
     for (i in 0..N-1) {
         if (i % 5 == 0) println()
         print("${arr[i]} ")
